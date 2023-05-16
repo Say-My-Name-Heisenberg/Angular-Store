3a508da571fce8ba3231a79eec94b56bc70a9dac
Sree-Ram-MS <srmsm3551@gmail.com>
2023-05-16 13:44:01 +0530

    regg

--- Store/src/app/new-pro/new-pro.component.ts
+++ Store/src/app/new-pro/new-pro.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder,Validator, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-pro',
@@ -10,18 +10,38 @@ export class NewProComponent {
 
   ProductForm=this.fb.group(
     {
-      title:'',
-      category:'',
-      desc:'',
-      rarting:'',
-      price:''
+      title:['',[Validators.required,Validators.pattern("[a-zA-Z ]+")]],
+      category:['',[Validators.required,Validators.pattern("[a-zA-Z ]+")]],
+      desc:['',[Validators.required,Validators.pattern("[a-zA-Z 0-9]+")]],
+      rarting:['',[Validators.required,Validators.pattern("[1-5]")]],
+      price:['',[Validators.required,Validators.pattern("[0-9]+")]]
     }
   )
 
   constructor(private fb:FormBuilder){}
 
   clicked(){
-    console.log(this.ProductForm.value)
-    console.log(this.ProductForm.value.title)
+    if (this.ProductForm.valid){
+      console.log(this.ProductForm.value)
+      console.log(this.ProductForm.value.title)
   }
+  else{
+    alert("invalid data")
+    if (this.ProductForm.get('title')?.invalid){
+      alert("Title is invalid")
+    }
+    if (this.ProductForm.get('category')?.invalid){
+      alert("Category is invalid")
+    }
+    if (this.ProductForm.get('desc')?.invalid){
+      alert("Description is invalid")
+    }
+    if (this.ProductForm.get('rating')?.invalid){
+      alert("Rating is invalid")
+    }
+    if (this.ProductForm.get('price')?.invalid){
+      alert("price is invalid")
+    }
+  }
+}
 }
